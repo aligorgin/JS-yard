93e0c8e085f99a7828f3f0a29e323633ee979394
ali <aliwolfi18@gmail.com>
2021-11-11 02:38:38 +0330

    testing git

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -2,7 +2,7 @@ import Parse from "../algorithms/parse";
 
 export default function App(){
     return(
-        <div>
+        <div className={'bg-red-500'}>
             <Parse/>
         </div>
     )
