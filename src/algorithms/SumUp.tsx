4511dad464bc114a2c0767e57de1adcee6024158
ali <aliwolfi18@gmail.com>
2021-11-26 18:43:15 +0330

    sum up changed

--- src/algorithms/SumUp.tsx
+++ src/algorithms/SumUp.tsx
@@ -1,23 +1,36 @@
 export default function SumUp() {
 
-    const sumUp = (n: number) => {
-        let result = 0;
-        for (let i = 1; i <= n; i++) {
-            result += i;
-        }
-        return result;
+    // const sumDown = (n: number) => {
+    //     let result = 0;
+    //     for (let i = 1; i <= n; i++) {
+    //         result += i; // 0(n)
+    //     }
+    //     return result;
+    // }
+
+    const sumDown = (n: number) => {
+        return (n / 2) * (1 + n); //o(1)
+    }
+    console.log(sumDown(3))
+
+    // const sumTheseNumbers = (numbers: []) => {
+    //     let result = 0;
+    //     for (const number of numbers) {
+    //         result += number;  //o(m)
+    //     }
+    //     return result;
+    // }
+
+    const sumTheseNumbers = (numbers: []) => {
+        return numbers.reduce((sum, curNum) => sum + curNum, 0)
     }
 
-    const sumUpWithGoodPerf = (n:number) => {
-        return (n / 2) * (1 + n);
-    }
+    console.log(sumTheseNumbers([10, 10, 10, 3]))
 
 
     return (
         <div>
-            {sumUp(3)}
-            <br/>
-            {sumUpWithGoodPerf(3)}
+            open the console
         </div>
     )
 }
