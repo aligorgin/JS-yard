47fd5925225b0f6e4ce4ad0e132b3db45ceaea6b
ali <aliwolfi18@gmail.com>
2021-12-30 14:15:15 +0330

    fix

--- src/algorithms/SmallestNumber.tsx
+++ src/algorithms/SmallestNumber.tsx
@@ -1,6 +1,6 @@
 export default function SmallestNumber() {
 
-    const getMin = (numbers: number[]) => {
+    const getMin = (numbers:number[]) => {
         let currentMin = numbers[0]
         for (const num of numbers) { // O(n)
             if (num < currentMin) {
@@ -9,6 +9,8 @@ export default function SmallestNumber() {
         }
         return currentMin;
     }
+
+    console.log(getMin([3,4,4,5,234,535345345,34534523452345,234523452345,234523452345,1]));
 
     return (
         <div>
