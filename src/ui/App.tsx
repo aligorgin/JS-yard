9bf161f46416d9e792cb0ceef755eb7cfd6f00bf
ali <aliwolfi18@gmail.com>
2022-01-01 21:19:39 +0330

    lil fix

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -18,7 +18,7 @@ export default function App() {
             {/*<SmallestNumber/>*/}
             {/*<OddEven/>*/}
             {/*<IsPowerOfTwo/>*/}
-            {/*<Factorial/>*/}
+            <Factorial/>
             <Permutations/>
         </div>
     )
