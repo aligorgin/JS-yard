55821e98c7c1cefef5a38ed6b5bc764f83c2ba97
ali <aliwolfi18@gmail.com>
2022-01-02 17:57:52 +0330

    fixing

--- src/algorithms/Permutations.tsx
+++ src/algorithms/Permutations.tsx
@@ -1,28 +1,28 @@
 export default function Permutations() {
 
     // without repetitions
-    const getPermutations = (options:  number[] | string[] ) => {
+    const getPermutations = (options: number[] | string[number] | { name: string; value: number; weight: number; }[]) => {
         const permutations = [];
         if (options.length === 1) {
-            return [options]
+            return [options];
         }
-        const partialPermutations = getPermutations(options.slice(1));
-        const firstOptions = options[0];
+        const partialPermutations: any = getPermutations(options.slice(1));
+        const firstOption = options[0];
         for (let i = 0; i < partialPermutations.length; i++) {
             const partialPermutation = partialPermutations[i];
 
-            for (let j = 0 ; j <= partialPermutation.length; i++){
-                const permutationInFront = partialPermutation.slice(0,j);
+            for (let j = 0; j <= partialPermutation.length; j++) {
+                const permutationInFront = partialPermutation.slice(0, j);
                 const permutationAfter = partialPermutation.slice(j);
-                permutations.push(permutationInFront.concat([firstOptions],permutationAfter))
-            };
+                permutations.push(permutationInFront.concat([firstOption], permutationAfter))
+            }
         }
+
+        return permutations;
     }
 
-    const x = getPermutations([3,4,5,6])
-    console.log(x)
-
-
+    const x = getPermutations([1, 3, 4, 5])
+    console.log(x);
 
     return (
         <div>
