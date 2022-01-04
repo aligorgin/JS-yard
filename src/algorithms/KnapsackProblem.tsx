2500b04a6816c78c2d1009f512d848294104548f
ali <aliwolfi18@gmail.com>
2022-01-04 22:44:53 +0330

    fixing knapsack

--- src/algorithms/KnapsackProblem.tsx
+++ src/algorithms/KnapsackProblem.tsx
@@ -1,27 +1,43 @@
 export default function KnapsackProblem() {
 
-    const knapsack: any = (items: any, cap: number, itemIndex: number) => {
+    const knapsackFn: any = (items: any, cap: number, itemIndex: number, memo: any) => {
+        console.log('Running');
+        if (memo[cap][itemIndex]) {
+            return memo[cap][itemIndex];
+        }
         if (cap === 0 || itemIndex < 0) {
             return {items: [], value: 0, weight: 0};
         }
         if (cap < items[itemIndex].weight) {
-            return knapsack(items, cap, itemIndex - 1);
+            return knapsackFn(items, cap, itemIndex - 1, memo);
         }
-        const sackWithItem: any = knapsack(items, cap - items[itemIndex].weight, itemIndex - 1);
-        const sackWithoutItem: any = knapsack(items, cap, itemIndex - 1);
+        const sackWithItem: any = knapsackFn(items, cap - items[itemIndex].weight, itemIndex - 1, memo);
+        const sackWithoutItem: any = knapsackFn(items, cap, itemIndex - 1, memo);
 
         const valueWithItem = sackWithItem.value + items[itemIndex].value;
         const valueWithoutItem = sackWithoutItem.value;
 
+        let resultSack;
+
         if (valueWithItem > valueWithoutItem) {
-            return {
+            const updatedSack = {
                 items: sackWithItem.items.concat(items[itemIndex]),
                 value: sackWithItem.value + items[itemIndex].value,
                 weight: sackWithItem.weight + items[itemIndex].weight,
-            }
+            };
+            resultSack = updatedSack;
         } else {
-            return sackWithoutItem;
+            resultSack = sackWithoutItem;
         }
+
+        memo[cap][itemIndex] = resultSack;
+
+        return resultSack;
+    }
+
+    const knapsack = (items: any, cap: number, index: number) => {
+        const mem = Array(cap + 1).fill(Array(items.length)).fill(undefined);
+        return knapsackFn(items, cap, index, mem);
     }
 
     const items = [
