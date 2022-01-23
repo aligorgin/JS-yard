11aebf3bb2a6782f5fb38679575e3f89867be91e
ali <aliwolfi18@gmail.com>
2022-01-23 21:48:22 +0330

    lil change

--- src/tik/Rows.tsx
+++ src/tik/Rows.tsx
@@ -14,10 +14,12 @@ export default function Rows({arr}: Props) {
         <div className='mt-4'>
             {arr.map((value: string | number, index: number) => {
                 return (
+
                     <div className={clsx('flex items-center text-white py-2 px-4 bg-slate-700 rounded-lg mb-2',checked &&'underline text-opacity-40')} key={index}>
-                        <input className='p-2' type="checkbox" value={checked} onClick={()=>{setChecked(!checked)}}/>
+                        <input className='mr-3' type="checkbox" value={checked} onClick={()=>{setChecked(!checked)}}/>
                         <div className='' >{value}</div>
                     </div>
+
                 )
             })}
         </div>
