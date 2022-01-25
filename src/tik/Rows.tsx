aafcfaf18b9708e67ba02659713537dae3f4c63b
ali <aliwolfi18@gmail.com>
2022-01-25 21:55:15 +0330

    seeking solution

--- src/tik/Rows.tsx
+++ src/tik/Rows.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function Rows({arr}: Props) {
     const [checked, setChecked] = useState<any>(false);
-    // const [selectedIndex, setSelectedIndex] = useState<SetStateAction<number | null>>(null);
+    const [selectedIndex, setSelectedIndex] = useState<SetStateAction<number | null>>(null);
 
     return (
         <div className='mt-4'>
@@ -15,15 +15,16 @@ export default function Rows({arr}: Props) {
                 return (
                     <div
                         key={index}
-                        // onClick={() => {
-                        //     setSelectedIndex(index);
-                        //     console.log('index',index,'selected index:',selectedIndex)
-                        // }}
+                        onClick={() => {
+                            setSelectedIndex(index);
+                            console.log('index',index,'selected index:',selectedIndex)
+                        }}
                         className={clsx('flex items-center text-white py-2 px-4 bg-slate-700 rounded-lg mb-2', checked && 'underline text-opacity-40')}
                         >
                         <input className='mr-3' type="checkbox" value={checked} onClick={() => {
                             setChecked(!checked)
                             console.log('index',index,'clicked')
+                            setSelectedIndex(index);
                         }}/>
                         <div className=''>{value}</div>
                     </div>
