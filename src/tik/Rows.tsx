f9bfcf2564076233dad222333bac194eb309ea57
ali <aliwolfi18@gmail.com>
2022-01-26 23:06:43 +0330

    lil change

--- src/tik/Rows.tsx
+++ src/tik/Rows.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function Rows({arr}: Props) {
     const [checked, setChecked] = useState<any>(false);
-    const [selectedIndex, setSelectedIndex] = useState<SetStateAction<number | null>>(null);
+    const [selectedIndex, setSelectedIndex] = useState<SetStateAction<number | null | number[]>>(null);
 
     return (
         <div className='mt-4'>
@@ -17,13 +17,13 @@ export default function Rows({arr}: Props) {
                         key={index}
                         onClick={() => {
                             setSelectedIndex(index);
-                            console.log('index',index,'selected index:',selectedIndex)
+                            console.log('index', index, 'selected index:', selectedIndex)
                         }}
                         className={clsx('flex items-center text-white py-2 px-4 bg-slate-700 rounded-lg mb-2', checked && 'underline text-opacity-40')}
-                        >
+                    >
                         <input className='mr-3' type="checkbox" value={checked} onClick={() => {
                             setChecked(!checked)
-                            console.log('index',index,'clicked')
+                            console.log('index', index, 'clicked')
                             setSelectedIndex(index);
                         }}/>
                         <div className=''>{value}</div>
