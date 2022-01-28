c55b25b9861fb6489174c2bbaf86e870f3b47c24
ali <aliwolfi18@gmail.com>
2022-01-28 23:42:13 +0330

    closure good example

--- src/js/Basics.tsx
+++ src/js/Basics.tsx
@@ -8,6 +8,16 @@ export default function Basics() {
     const c = {d: [true, false]}
     console.log({a, b, c})
 
+    const makeAdder = (x: number) => {
+        return (y: number) => {
+            return x + y;
+        }
+    }
+    let add3 = makeAdder(3);
+    let add7 = makeAdder(7);
+    console.log(add3(2));
+    console.log(add7(7));
+
     return (
         <div>
             open consolo
