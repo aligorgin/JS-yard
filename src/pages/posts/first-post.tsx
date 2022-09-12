432db4065c64571b4b19888a4d5681ca71fdec73
ali <aliwolfi18@gmail.com>
2022-09-12 06:51:24 +0430

    migrate from webstorm to vs code

--- src/pages/posts/first-post.tsx
+++ src/pages/posts/first-post.tsx
@@ -1,9 +1,20 @@
 import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
 
 export default function FirstPost() {
+
+	const firaCode = () => {
+
+	}
+
 	return (
 		<>
+			<Head>
+				<title>First Post</title>
+			</Head>
 			<h1>First Post</h1>
+			<Image src={'/images/profile.jpg'} width={144} height={144} alt={'test pic'} />
 			<h2>
 				<Link href={'/'}>Back to home</Link>
 			</h2>
