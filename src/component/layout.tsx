53a21d60de69d230936cd016664244fa8d0aeed2
ali <aliwolfi18@gmail.com>
2022-10-08 07:39:26 +0330

    added id

--- src/component/layout.tsx
+++ src/component/layout.tsx
@@ -13,63 +13,61 @@ interface Props {
 
 export default function Layout({ children, home }: Props) {
 	return (
-		<div className="max-w-xl w-full mx-auto mt-12">
-				<Head>
-					<link rel="icon" href="/favicon.ico" />
-					<meta name="description" content="Learn how to build a personal webstite using next.js" />
-					<meta
-						property="og:image"
-						content={`https://og-image.vercel.app/${encodeURI(
-							siteTitle
-						)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-					/>
-					<meta name="og:title" content={siteTitle} />
-				</Head>
-				<header className="flex flex-col items-center">
-					{home ? (
-						<>
-							<Image
-								priority
-								src={'/images/profile.jpg'}
-								className="rounded-full"
-								height={148}
-								width={148}
-								alt={name}
-							/>
-							<h1 className="font-[2.5rem] leading-[1.2] font-extrabold tracking-[-0.05rem] my-4 mx-0">
-								{name}
-							</h1>
-						</>
-					) : (
-						<>
-							<Link href={'/'}>
-								<a>
-									<Image
-										priority
-										src={'/images/profile.jpg'}
-										className="rounded-full"
-										height={108}
-										width={108}
-										alt={name}
-									/>
-								</a>
-							</Link>
-							<h2 className="text-[1.5rem] leading-[1.4] my-4 mx-0">
-								<Link href={'/'}>
-									<a className='text-inherit'>{name}</a>
-								</Link>
-							</h2>
-						</>
-					)}
-				</header>
-				<main>{children}</main>
-				{!home && (
-					<div className='mt-12 mx-0 mb-0'>
+		<div className="mx-auto mt-12 w-full max-w-xl">
+			<Head>
+				<link rel="icon" href="/favicon.ico" />
+				<meta name="description" content="Learn how to build a personal webstite using next.js" />
+				<meta
+					property="og:image"
+					content={`https://og-image.vercel.app/${encodeURI(
+						siteTitle
+					)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+				/>
+				<meta name="og:title" content={siteTitle} />
+			</Head>
+			<header className="flex flex-col items-center">
+				{home ? (
+					<>
+						<Image
+							priority
+							src={'/images/profile.jpg'}
+							className="rounded-full"
+							height={148}
+							width={148}
+							alt={name}
+						/>
+						<h1 className="font-[2.5rem] my-4 mx-0 font-extrabold leading-[1.2] tracking-[-0.05rem]">
+							{name}
+						</h1>
+					</>
+				) : (
+					<>
 						<Link href={'/'}>
-							&#60;- Back to home
+							<a>
+								<Image
+									priority
+									src={'/images/profile.jpg'}
+									className="rounded-full"
+									height={108}
+									width={108}
+									alt={name}
+								/>
+							</a>
 						</Link>
-					</div>
+						<h2 className="my-4 mx-0 text-[1.5rem] leading-[1.4]">
+							<Link href={'/'}>
+								<a className="text-inherit">{name}</a>
+							</Link>
+						</h2>
+					</>
 				)}
-			</div>
+			</header>
+			<main>{children}</main>
+			{!home && (
+				<div className="mx-0 mt-12 mb-0 text-blue-200">
+					<Link href={'/'}>&#60;- Back to home</Link>
+				</div>
+			)}
+		</div>
 	);
 }
--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,17 +1,31 @@
-import { getAllPostIds } from "../../../lib/posts";
-import Layout from "../../component/layout";
+import { getAllPostIds, getPostData } from '../../../lib/posts';
+import Layout from '../../component/layout';
 
-export async function getStaticPaths() {
-   const paths = getAllPostIds();
-   return {
-      paths,
-      fallback:false,
-   };
+export async function getStaticProps({ params }: any) {
+	const postData = getPostData(params.id);
+	return {
+		props: {
+			postData
+		}
+	};
 }
 
+export async function getStaticPaths() {
+	const paths = getAllPostIds();
+	return {
+		paths,
+		fallback: false
+	};
+}
 
-export default function Post() {
-   return (
-      <Layout>...</Layout>
-   )
+export default function Post({ postData }: any) {
+	return (
+		<Layout>
+			{postData.title}
+			<br />
+			{postData.id}
+			<br />
+			{postData.date}
+		</Layout>
+	);
 }
